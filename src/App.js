a6d0d1b90d2b50546abd35b145b07b01f5185865
raisuahmed <raisuahmed83@gmail.com>
2020-10-17 14:06:48 +0600

    working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Home from './Components/Home/Home';
+
+  export const userContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
-      
-    </div>
+    <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route></Switch>
+      </Router>
+    </userContext.Provider>
   );
 }
 
